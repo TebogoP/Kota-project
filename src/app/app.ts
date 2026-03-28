0e73ed4fe2b30c60767e6163dd6da7d336c155db
Tebogo <tphiri.firi@gmail.com>
2026-03-28 10:09:43 +0200

    feat: added navbar component - No styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { NavComponent } from './nav-component/nav-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Home, NavComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('Kota-project');
