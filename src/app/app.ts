2b4556d786e7e96c9f132e6d76eab5c61a97824d
Tebogo <tphiri.firi@gmail.com>
2026-03-28 16:13:23 +0200

    style: added navbar styling for logo and links

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
 import { NavComponent } from './nav-component/nav-component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [Home, NavComponent],
+  imports: [Home, NavComponent, NgOptimizedImage],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
